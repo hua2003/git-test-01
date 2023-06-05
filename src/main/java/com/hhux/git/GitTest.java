65fdaf958d8b0c293c3b7e0435b7eb9b13b18323
yynx <2182523056@qq.com>
2023-06-05 09:53:37 +0800

    push commit

--- src/main/java/com/hhux/git/GitTest.java
+++ src/main/java/com/hhux/git/GitTest.java
@@ -3,9 +3,9 @@ package com.hhux.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-
         System.out.println("master test");
-
         System.out.println("hot-fix test");
+
+        System.out.println("push 111");
     }
 }
